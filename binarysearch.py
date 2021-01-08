bcbcfff13afac668c1f86b28de7d9ccc00393ce6
kuljotbiring <34665034+kuljotbiring@users.noreply.github.com>
2021-01-07 21:01:34 -0800

    Update binarysearch.py

--- binarysearch.py
+++ binarysearch.py
@@ -7,7 +7,7 @@ you're searching for.
 Assume the list only has distinct elements,
 meaning there are no repeated values, and
 elements are in a strictly increasing order.
-Return the index of value, or -1 if the value
+Return the index of value, or a -1 if the value
 doesn't exist in the list."""
 
 
